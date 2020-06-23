42aa7ad4dd783970ed3be02de7d36a410c544fbf
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-23 15:32:34 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -14,8 +14,9 @@ class GetRequester
   end
    
   def get_response_body
-   uri = URI.parse(url)
-   Net::HTTP.get_response(uri)
+     uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    response.body
   end
   
   
