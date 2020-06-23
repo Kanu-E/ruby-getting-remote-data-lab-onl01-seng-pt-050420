5bd1aef0a77210ea828f6b895e328f63a53cedbc
Emeka Kanu <chukwuemekakanu@gmail.com>
2020-06-23 15:03:16 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,19 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+  
+  attr_accessor :url
+  
+  def initialize(url)
+  @url = url
+  end
+   
+  def get_response_body
+    url
+  end
+  
+  
+end
